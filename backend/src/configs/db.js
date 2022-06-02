2c5b84928a8618535f4e6cb23eaf8249bac348cc
udit kishor <uditkishorofficial@gmail.com>
2022-06-02 20:02:44 +0530

    delete backend

--- backend/src/configs/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = () => {
-  return mongoose.connect(
-    "mongodb+srv://udit:udit@cluster0.n9ur0.mongodb.net/fakeshop?retryWrites=true&w=majority"
-  );
-};
