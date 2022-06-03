6bab8ba2abb3477989f10ac642094e9a0522509f
Udit kishor <uditkishorofficial@gmail.com>
2022-06-03 12:04:29 +0530

    added get method for users

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const User = require("../models/user.model");
+const crudController = require("./crud.controllers");
 
 const router = express.Router();
+
+router.get("", crudController(User).getAll);
+
+router.patch("/:id", crudController(User).updateOne);
+
+router.delete("/:id", crudController(User).deleteOne);
 
 module.exports = router;
