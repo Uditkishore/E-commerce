86bcafac6b6638e896d92b910d44c6cfb344a1a1
Udit kishor <uditkishorofficial@gmail.com>
2022-06-06 20:17:38 +0530

    update

--- backend/src/models/cart.module.js
+++ backend/src/models/cart.module.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cartSchema = new mongoose.Schema(
   {
     id: { type: Number, required: false },
+    userid: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
     rname: { type: String, required: true },
     imgdata: { type: String, required: true },
     address: { type: String, required: true },
